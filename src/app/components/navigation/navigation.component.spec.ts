aa39ff2a1c76d69c6f73e5d4679af63997063f84
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2025-11-07 17:18:58 -0500

    bugfix: add required providers to Angular component tests
    - Add provideRouter, provideHttpClient, and provideAuth0 to AppComponent test
    - Add provideRouter, provideHttpClient, and provideAuth0 to NavigationComponent test
    - Fixes NG0201 errors for ActivatedRoute and Auth0 providers
    - All Angular tests now passing (7/7)

--- src/app/components/navigation/navigation.component.spec.ts
+++ src/app/components/navigation/navigation.component.spec.ts
@@ -1,3 +1,6 @@
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavigationComponent } from './navigation.component';
@@ -9,6 +12,17 @@ describe('NavgiationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NavigationComponent],
+      providers: [
+        provideHttpClient(),
+        provideRouter([]),
+        provideAuth0({
+          domain: 'test.auth0.com',
+          clientId: 'test-client-id',
+          authorizationParams: {
+            redirect_uri: 'http://localhost:4200',
+          },
+        }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavigationComponent);
